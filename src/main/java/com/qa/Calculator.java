5ab3d85aec81dea731f05ee4a412a8c2d97af6b7
Kyle P <kprestondeveloper@gmail.com>
2023-10-26 11:50:37 +0100

    Added divide func and tests

--- src/main/java/com/qa/Calculator.java
+++ src/main/java/com/qa/Calculator.java
@@ -5,4 +5,14 @@ public class Calculator {
     public static int add(int num1, int num2) {
         return num1 + num2;
     }
+
+    public static double divide(double num1, double num2)
+    {
+        return num1/num2;
+    }
+
+    public static int divide(int num1, int num2)
+    {
+        return num1/num2;
+    }
 }
--- src/test/java/com/qa/CalculatorTest.java
+++ src/test/java/com/qa/CalculatorTest.java
@@ -1,5 +1,7 @@
 package com.qa;
 
+import jdk.jfr.Label;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -37,4 +39,36 @@ class CalculatorTest {
         assertEquals(expectedResult, actualResult);
     }
 
+    @Test
+    @DisplayName("Divide 10 by 2")
+    public void divide_10_by_2() {
+        double num1 = 10;
+        double num2 = 2;
+        double expectedResult = 5;
+
+        double actualResult = Calculator.divide(num1, num2);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("Divide by double 0 gives infinity")
+    public void divide_by_double_0_gives_infinity() {
+        double num1 = 5;
+        double num2 = 0;
+        double expectedResult = Double.POSITIVE_INFINITY;
+
+        double actualResult = Calculator.divide(num1, num2);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("Divide by double 0 gives infinity")
+    public void divide_by_int_0_gives_infinity() {
+        int num1 = 5;
+        int num2 = 0;
+
+        assertThrows(ArithmeticException.class, () -> Calculator.divide(num1, num2));
+    }
 }
